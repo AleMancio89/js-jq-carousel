18d2d7aa6605804910db5989c0eb6ad5154eea81
Alessandro <alessandro.mancinelli89@gmail.com>
2020-12-30 10:06:53 +0100

    added exercise text

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+/*
+Creare uno slider di immagini
+Potete usare le immagini che desiderate.
+Per facilitarvi la vita usate immagini delle stesse dimensioni :)
+Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider
+Per scorrere le immagini permettere anche l'uso delle frecce sinistra e destra della tastiera ( e' un evento diverso dal click, quale? )
+Utiliziamo una classe first e last  per capire quali sono la prima e ultima immagine dello slider
+Utilizziamo una classe active per aiutarci a capire quale è l'immagine attuale da visualizzare nello slider
+Bonus:
+Clicchiamo sui pallini e mostriamo l'immagine corrispondente
+Generiamo i pallini con JS
+*/
+
 
 $(document).ready(function(){
 
@@ -43,30 +56,29 @@ $(document).ready(function(){
   $('.next').click(moveOn)
   $('.prev').click(moveBack)
 
+  //Seleziono le foto al click dei pallini
+
+  $('.nav > i').click(function(){
+    var selectedElementIndex = $(this).index()
+
+    $(this).addClass('active')
+    $(this).siblings().removeClass('active')
+
+    var selectedImgEl = $('.images > img').eq(selectedElementIndex)
+
+    selectedImgEl.addClass('active')
+    selectedImgEl.siblings().removeClass('active')
+  })
+
   // Imposto lo slide tramite la pressione delle frecce sulla tastiera
 
   $(document).keydown(function(event){
-    if (event.which == 39||event.keyCode == 39) {
+    if (event.which == 39) {
       moveOn()
-    } else if (event.which == 37||event.keyCode == 37) {
+    } else if (event.which == 37) {
       moveBack()
     }
   })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 })
